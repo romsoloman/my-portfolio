9c78ffdb7c4e26fb198428d8ca1447eb5ad3e0b7
Rom Soloman <romsoloman19@gmail.com>
2021-02-01 18:02:58 +0200

    change render Modals

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ function renderProjects() {
     var strHTMLs = projects.map(function (project) {
         return `
           <div class="col-md-4 col-sm-6 portfolio-item">
-            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1" onclick = "(updateCurrProject(${project.id}))">>
                <div class="portfolio-hover">
                  <div class="portfolio-hover-content">
                    <i class="fa fa-plus fa-3x"></i>
@@ -30,8 +30,25 @@ function renderProjects() {
         `
     })
     var $elRow = $('#portfolio .projects');
-    console.log('$elRow', $elRow);
     $elRow.html(strHTMLs.join(''));
 }
 
-
+function renderModal() {
+    var currProject = getCurrProject();
+    var date = new Date(currProject.publishedAt);
+    var strHTML = `
+      <h2>${currProject.name}</h2>
+      <p class="item-intro text-muted">${currProject.title}.</p>
+      <img class="img-fluid d-block mx-auto" src="${currProject.imgUrl}">
+      <p>${currProject.desc}</p>
+      <ul class="list-inline">
+        <li>Date: ${date.toString()}</li>
+      </ul>
+      <a href = "${currProject.url}" target ="_blank" style = "display:block">Link to My project</a>
+      <button class="btn btn-primary" data-dismiss="modal" type="button">
+        <i class="fa fa-times"></i>
+        Close Project
+      </button>`;
+    var $elModalBody = $('.modal-body');
+    $elModalBody.html(strHTML);
+}
--- js/service-gallery.js
+++ js/service-gallery.js
@@ -1,8 +1,8 @@
 'use strict'
-
+var gCurrProj = null;
 var gProjects = [
     {
-        id: 'safe-content',
+        id: 1,
         name: 'Safe-content',
         title: 'Storage your data secretly',
         desc: 'If You want a secret place to store your data , you chose the right place, we are save your password in a special storage.',
@@ -12,17 +12,17 @@ var gProjects = [
         labels: ['Log in', 'Admin\'s Page'],
     },
     {
-        id: 'book-shop',
+        id: 2,
         name: 'Book-Shop',
         title: 'Designed smart book system',
         desc: 'A smart book system that uses the new CRUDL principles learned.With it you can add new books, delete, update and also see information and ratings about the book you have chosen',
         imgUrl: 'img/portfolio/book-shop.jpg',
         url: 'https://romsoloman.github.io/Book-Shop/',
-        publishedAt: 1448693940000,
+        publishedAt: 144869320000,
         labels: ['CRUDL', 'Book System'],
     },
     {
-        id: 'chess',
+        id: 3,
         name: 'Chess',
         title: 'Chess is a competitive board game.',
         desc: 'Chess is a board game for two players. It is played in a square board, made of 64 smaller squares, with eight squares on each side. Each player starts with sixteen pieces: eight pawns, two knights, two bishops, two rooks, one queen and one king.[2] The goal of the game is for each player to try and checkmate the king of the opponent. Checkmate is a threat  to the opposing king which no move can stop. It ends the game.',
@@ -32,7 +32,7 @@ var gProjects = [
         labels: ['arcade'],
     },
     {
-        id: 'mine-sweapper',
+        id: 4,
         name: 'Mine-sweapper',
         title: 'Minesweeper is a single-player puzzle video game.',
         desc: 'The objective of the game is to clear a rectangular board containing hidden "mines" or bombs without detonating any of them, with help from clues about the number of neighboring mines in each field. The game originates from the 1960s, and it has been written for many computing platforms in use today. It has many variations and offshoots.',
@@ -42,7 +42,7 @@ var gProjects = [
         labels: ['Arcade'],
     },
     {
-        id: 'touch-nums',
+        id: 5,
         name: 'Touch-nums',
         title: 'A competitive game for learning the principles of numbers',
         desc: 'A competitive game for learning the principles of numbers',
@@ -52,7 +52,7 @@ var gProjects = [
         labels: ['Arcade', 'Studies'],
     },
     {
-        id: 'pacman',
+        id: 6,
         name: 'Pacman',
         title: 'Pac-Man is a maze arcade game.',
         desc: 'The original Japanese title of Puck Man was changed to Pac-Man for international releases as a preventative measure against defacement of the arcade machines by changing the P to an F.[1] Outside Japan, the game was published by Midway Games as part of its licensing agreement with Namco America. The player controls Pac-Man, who must eat all the dots inside an enclosed maze while avoiding four colored ghosts. Eating large flashing dots called "Power Pellets" causes the ghosts to turn blue, allowing Pac-Man to eat them for bonus points.',
@@ -66,4 +66,15 @@ var gProjects = [
 
 function getProjects() {
     return gProjects;
+}
+
+function updateCurrProject(id) {
+    var project = gProjects.find(function (project) {
+        return id === project.id;
+    })
+    gCurrProj = project;
+    renderModal();
+}
+function getCurrProject() {
+    return gCurrProj;
 }
