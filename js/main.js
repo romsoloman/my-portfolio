14418a494e71e47cc3270898e6962fd38d96ca3a
Rom Soloman <romsoloman19@gmail.com>
2021-02-01 16:37:39 +0200

    render projects

--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
 console.log('Starting up');
+
+$(document).ready(initPage);
+
+function initPage() {
+    console.log('Started...');
+    renderProjects();
+}
+
+
+function renderProjects() {
+    var projects = getProjects();
+
+    var strHTMLs = projects.map(function (project) {
+        return `
+          <div class="col-md-4 col-sm-6 portfolio-item">
+            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+               <div class="portfolio-hover">
+                 <div class="portfolio-hover-content">
+                   <i class="fa fa-plus fa-3x"></i>
+                 </div>
+               </div>
+              <img class="img-fluid imgs" src="${project.imgUrl}" alt="">
+            </a>
+           <div class="portfolio-caption">
+              <h4>${project.name}</h4>
+              <p class="text-muted">${project.title}</p>
+           </div>
+          </div> 
+        `
+    })
+    var $elRow = $('#portfolio .projects');
+    console.log('$elRow', $elRow);
+    $elRow.html(strHTMLs.join(''));
+}
+
+
---
+++ js/service-gallery.js
@@ -0,0 +1,69 @@
+'use strict'
+
+var gProjects = [
+    {
+        id: 'safe-content',
+        name: 'Safe-content',
+        title: 'Storage your data secretly',
+        desc: 'If You want a secret place to store your data , you chose the right place, we are save your password in a special storage.',
+        imgUrl: 'img/portfolio/safe-content.jpeg',
+        url: 'https://romsoloman.github.io/Secret-Content/',
+        publishedAt: 1448693940000,
+        labels: ['Log in', 'Admin\'s Page'],
+    },
+    {
+        id: 'book-shop',
+        name: 'Book-Shop',
+        title: 'Designed smart book system',
+        desc: 'A smart book system that uses the new CRUDL principles learned.With it you can add new books, delete, update and also see information and ratings about the book you have chosen',
+        imgUrl: 'img/portfolio/book-shop.jpg',
+        url: 'https://romsoloman.github.io/Book-Shop/',
+        publishedAt: 1448693940000,
+        labels: ['CRUDL', 'Book System'],
+    },
+    {
+        id: 'chess',
+        name: 'Chess',
+        title: 'Chess is a competitive board game.',
+        desc: 'Chess is a board game for two players. It is played in a square board, made of 64 smaller squares, with eight squares on each side. Each player starts with sixteen pieces: eight pawns, two knights, two bishops, two rooks, one queen and one king.[2] The goal of the game is for each player to try and checkmate the king of the opponent. Checkmate is a threat  to the opposing king which no move can stop. It ends the game.',
+        imgUrl: 'img/portfolio/chess.jpeg',
+        url: 'https://romsoloman.github.io/chess/',
+        publishedAt: 1448693940000,
+        labels: ['arcade'],
+    },
+    {
+        id: 'mine-sweapper',
+        name: 'Mine-sweapper',
+        title: 'Minesweeper is a single-player puzzle video game.',
+        desc: 'The objective of the game is to clear a rectangular board containing hidden "mines" or bombs without detonating any of them, with help from clues about the number of neighboring mines in each field. The game originates from the 1960s, and it has been written for many computing platforms in use today. It has many variations and offshoots.',
+        imgUrl: 'img/portfolio/mine-sweapper.jpeg',
+        url: 'https://romsoloman.github.io/Mine-Sweaper-Rom-soloman/',
+        publishedAt: 1448693940000,
+        labels: ['Arcade'],
+    },
+    {
+        id: 'touch-nums',
+        name: 'Touch-nums',
+        title: 'A competitive game for learning the principles of numbers',
+        desc: 'A competitive game for learning the principles of numbers',
+        imgUrl: 'img/portfolio/touch-nums.jpeg',
+        url: 'https://romsoloman.github.io/touch-nums/',
+        publishedAt: 1448693940000,
+        labels: ['Arcade', 'Studies'],
+    },
+    {
+        id: 'pacman',
+        name: 'Pacman',
+        title: 'Pac-Man is a maze arcade game.',
+        desc: 'The original Japanese title of Puck Man was changed to Pac-Man for international releases as a preventative measure against defacement of the arcade machines by changing the P to an F.[1] Outside Japan, the game was published by Midway Games as part of its licensing agreement with Namco America. The player controls Pac-Man, who must eat all the dots inside an enclosed maze while avoiding four colored ghosts. Eating large flashing dots called "Power Pellets" causes the ghosts to turn blue, allowing Pac-Man to eat them for bonus points.',
+        imgUrl: 'img/portfolio/pacman.jpeg',
+        url: 'https://romsoloman.github.io/pacman/',
+        publishedAt: 1448693940000,
+        labels: ['Arcade'],
+    },
+]
+
+
+function getProjects() {
+    return gProjects;
+}
